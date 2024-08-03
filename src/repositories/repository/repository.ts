301c83d73a844cc73ff128fa9ced0b8068a17176
Maeve <foldybox.dot@gmail.com>
2024-08-03 17:35:43 +0200

    Updated lint errors

--- src/repositories/repository/repository.ts
+++ src/repositories/repository/repository.ts
@@ -278,6 +278,6 @@ export class Repository<TEntity extends Entity> {
 
   // ! Temporary deactivated
   protected encodeRFC3986URI(str: string) {
-    return str; //encodeURI(str).replace(/['-]/g, c => `%${c.charCodeAt(0).toString(16).toUpperCase()}`);
+    return str; // encodeURI(str).replace(/['-]/g, c => `%${c.charCodeAt(0).toString(16).toUpperCase()}`);
   }
 }
